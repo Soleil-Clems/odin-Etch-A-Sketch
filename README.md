07d773d8107f9c3c46a22585af4687ed287f50df
Soleil-Clems <ouisolclems@gmail.com>
2023-04-17 12:56:05 +0200

    Add all necesary file and modifiying the Readme file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # odin-Etch-A-Sketch
 Odin Project Etch-A-Sketch
+
+-Le Projet Etch-A-Sketch consiste a changer la couleur de chaque grid lorsque la souris fait un hover. Les grilles doivent etre dynamiquement creees selon les tailles que choisirons les utilisateurs.
+
+-Vous disposerez de trois boutons qui vous permettrons de choisir les couleurs et de tout effacer si vous le souhaitez
+
+-Le projet Etch-A-Sketch fait intervenir le concept de la manipulation du DOM, mais egalement l'utilisation des fonctions
---
+++ script.js
@@ -0,0 +1,114 @@
+let container = document.querySelector('#container')
+const range = document.getElementById('range')
+const chooseColor = document.getElementById('chooseColor')
+const clear = document.getElementById('clear') 
+const eraser = document.getElementById('eraser')
+const rainbow =document.getElementById('rainbow')
+
+let getColor= ()=> {
+    return window.getcolor || `hsla(${Math.floor(Math.random()* 360)}, 100%, 50%, ${Math.random()} )`
+}
+
+let bgOfOneGrid=()=>{
+    let grid = document.querySelectorAll('.grids')
+    grid.forEach(g => {
+        g.addEventListener('mouseover', ()=>{
+            g.style.background = getColor()
+        })
+        
+        
+    });
+}
+
+let n = range.value
+makeGrid(n)
+
+
+function createGrid(){
+    const grids = document.createElement('button')
+    grids.classList = "grids"
+    container.appendChild(grids)
+    
+}
+
+function numberOfGrids(number) {
+    container.style.gridTemplateColumns = `repeat(${number}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${number}, 1fr)`
+    
+}
+
+
+function makeGrid(number){
+    for(let i = 0; i < number**2; i++){
+        createGrid()
+        
+    }
+    numberOfGrids(number)
+    bgOfOneGrid()
+    
+}
+
+function clearGrids() {
+    let grid = document.querySelectorAll('.grids')
+    
+    for(let i=0; i<grid.length; i++){
+        grid[i].style.background="white"
+    }
+}
+// console.log(container);
+range.addEventListener('input', ()=>{
+    clearGrids()
+    n= range.value
+    makeGrid(n)
+    // console.log(container);
+    // console.log(container);
+})
+
+rainbow.addEventListener('click', ()=>{
+    monoColor.classList.remove('active')
+    clear.classList.remove('active')
+    eraser.classList.remove('active')
+    rainbow.classList.add('active')
+    if (rainbow.classList[1] == 'active') {
+        getColor=()=>{
+            return `hsla(${Math.floor(Math.random()* 360)}, 100%, 50%, ${Math.random()} )`
+        }
+    }
+    makeGrid(n)
+    
+})
+eraser.addEventListener('click', ()=>{
+    
+    clear.classList.remove('active')
+    rainbow.classList.remove('active')
+    monoColor.classList.remove('active')
+    eraser.classList.add('active')
+    if (eraser.classList[1] == 'active') {
+        getColor=()=>{
+            return monoColor.value
+        }
+    }
+    makeGrid(n)
+    
+})
+
+monoColor.addEventListener('click', ()=>{
+    
+    clear.classList.remove('active')
+    eraser.classList.remove('active')
+    rainbow.classList.remove('active')
+    monoColor.classList.add('active')
+    if (monoColor.classList[1] == 'active') {
+        getColor=()=>{
+            return chooseColor.value
+        }
+    }
+    makeGrid(n)
+    
+})
+
+clear.addEventListener('click', ()=>{
+    clearGrids()
+})
+
+
